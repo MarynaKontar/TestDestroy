5046a259c4a845ff72b6e5e585d5299b94d9fc15
mkont <mkont@softserveinc.com>
2020-06-12 19:08:13 +0300

    Initial commit

---
+++ TestDestroy/Person.cs
@@ -0,0 +1,78 @@
+﻿﻿using System;
+
+namespace TestWebApplication
+{
+    public class Person: IDisposable
+    {
+        public string FirstName { get; set; } 
+        
+        public string LastName { get; set; }
+        
+        public int Age { get; set; } 
+
+        public Person(string fn, string ln, int a)
+        {
+            this.FirstName = fn;
+            this.LastName = ln;
+            this.Age = a;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(FirstName)}: {FirstName}, " +
+                   $"{nameof(LastName)}: {LastName}, " +
+                   $"{nameof(Age)}: {Age}";
+        }
+
+//=====================================DESTRUCTOR=======================================================================        
+        //Освобождение неуправляемых ресурсов подразумевает реализацию одного из двух механизмов:
+        //Создание деструктора
+        //Реализация классом интерфейса System.IDisposable
+        
+        //https://www.c-sharpcorner.com/article/expression-bodied-members/
+        // DESTRUCTOR implemented using Expression Bodied  
+        // Expression or right side followed with => must be valid and should be one statement  
+        // Only assignment, call, increment, decrement, and new object expression can be used as a statement  
+        // This feature can only be used in C# 7.0 or onwards.
+        //https://metanit.com/sharp/tutorial/8.2.php
+
+        private bool disposed = false;
+ 
+        // реализация интерфейса IDisposable.
+        public void Dispose()
+        {
+            Dispose(true);
+            // подавляем финализацию
+            GC.SuppressFinalize(this);
+        }
+ 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Освобождаем управляемые ресурсы
+                    Console.WriteLine("Releases the managed resources");
+                    Console.Beep();
+                }
+                // освобождаем неуправляемые объекты
+                Console.WriteLine("Releases the unmanaged resources");
+                Console.Beep(); 
+                Console.Beep();
+                disposed = true;
+            }
+        }
+ 
+        // Деструктор (компилятор переделывает его в Finalize метод)
+        ~Person()
+        {
+            Dispose (false); // если не вызывать метод Destroy(), то все равно вызовется этот деструктор, если память будет сильно перегружена, и тогда освободятся все ресурсы (если здесь будет true) или только неуправляемые (если здесь будет false). !!! Надо подумать, когда буду использовать реальные ресурсы
+            
+            // https://metanit.com/sharp/tutorial/8.2.php
+            //При вызове деструктора в качестве параметра disposing передается значение false, чтобы избежать очистки
+            //управляемых ресурсов, так как мы не можем быть уверенными в их состоянии, что они до сих пор находятся
+            //в памяти. И в этом случае остается полагаться на деструкторы этих ресурсов
+        }
+    }
+}
---
+++ TestDestroy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TestWebApplication;
+
+namespace TestDestroy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//==========================================Destroy=====================================================================
+            //https://metanit.com/sharp/tutorial/8.2.php
+            //see destructor ~Person() and Destroy() method in Person
+            TestDestroyMethod();
+            
+            for (int i = 0; i < 100000; i++)
+            {
+                string str = new string("This loop forces GC collect garbage. 100_000 is enough for my laptop.");
+            }
+            // GC.Collect();
+        }
+        
+        private static void TestDestroyMethod()
+        {
+            Person pp = new Person("Ted", "Neward", 46);// в данном случае после вызова GC.Collect() (или достаточно большого for) пойдет в деструктор ~Person, так как Finalize не подавлен в Destroy() 
+
+            // аналог try with resources  в java
+            // если использовать using и потом вызвать GC.Collect(), то в деструктор (~Person) не пойдет так как подавили Finilize метод в Dispose()
+            // using (Person p = new Person("Ted", "Neward", 46)) // в данном случае вызывается метод Destroy() у Person (по аналогии с close() в java) 
+            // {
+            //     var pAge = p.Age;
+            // }
+        }
+    }
+
+}
